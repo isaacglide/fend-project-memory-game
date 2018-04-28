e496086c8ba74cb989261f015cd6eed9e93b7cee
Isaac Glidewell <isaacglide@gmail.com>
2018-04-28 14:47:50 -0500

    Flips card if clicked

--- js/app.js
+++ js/app.js
@@ -60,3 +60,14 @@ function shuffle(array) {
  *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
  *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
  */
+
+ gameBoard.addEventListener('click',displayCard);         //Add event lister if a card is clicked
+
+
+
+ function displayCard (event) {                           //Flip card function
+   if (event.target.nodeName === 'LI') {                  //Check that a card was the item clicked on screen
+        event.target.classList.add('open','show');        //Show card by adding the open and show classes to <li>
+   }
+
+ }
